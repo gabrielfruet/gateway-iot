43a11904e3bb91bfce1ab851c58dd5cf3c49afd7
gabrielfruet <gabrielfruet538@gmail.com>
2025-01-26 09:58:29 -0300

    fix: data is str now

--- devices/actuator.py
+++ devices/actuator.py
@@ -2,6 +2,10 @@ import main
 import grpc
 import proto.services_pb2_grpc as services_grpc
 import proto.services_pb2 as services
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class ActuatorServer(services_grpc.ActuatorServicer):
     def __init__(self, device: main.Device):
@@ -10,9 +14,10 @@ class ActuatorServer(services_grpc.ActuatorServicer):
 
     def ChangeState(self, request: services.ActuatorState, context):
         data = self.device.change_data(request)
+        logger.info(f"Data changed to {data}")
         if data is None:
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
             context.set_details("ID does not match")
-            return services.ActuatorState(id=self.device.id, state="0")
+            return services.ActuatorState(id=self.device.sensor_id, state="0")
 
-        services.ActuatorState(id=self.device.id, state=str(data))
+        return services.ActuatorState(id=self.device.sensor_id, state=str(data))
--- devices/main.py
+++ devices/main.py
@@ -3,8 +3,10 @@ import proto.messages_pb2 as messages
 import proto.services_pb2 as services
 import proto.services_pb2_grpc as services_grpc
 import actuator
+import uuid
 from concurrent import futures
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
+import pika.connection
 import grpc
 import sys
 import time
@@ -20,57 +22,82 @@ logger.setLevel(logging.INFO)
 logger.handlers.clear()
 logger.addHandler(syslog)
 
+broker_connect = Lock()
 
 def connect_to_broker() -> pika.BlockingConnection:
+    global broker_connect
+    broker_connect.acquire()
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    broker_connect.release()
     return connection
 
 class Device():
-    def __init__(self, name: str, data: int):
-        self.connection: pika.BlockingConnection = connect_to_broker() 
-        self.id: str | None = None
+    def __init__(self, name: str, ip: str, port: str, data: int):
+        self.sensor_id: str = str(uuid.uuid4())
+        self.actuator_id: str = ""
+        self.port = port
+        self.ip = ip
         self.name = name
         self.data: int = data
         self.data_lock: Lock = Lock()
 
     def start(self):
-        self._connect_to_gateway()
-        t = Thread(target=self.send_to_gateway)
-        t.run()
+        t1 = Thread(target=self._connect_actuator_to_gateway)
+        t2 = Thread(target=self.send_to_gateway)
+        t1.start()
+        t2.start()
 
-    def _connect_to_gateway(self):
-        channel = self.connection.channel()
+    def _connect_actuator_to_gateway(self):
+        connection: pika.BlockingConnection = connect_to_broker() 
+        channel = connection.channel()
+        try:
+            while True:
+                logger.info("Registering actuator")
+                exchange_name = 'actuator_registration_order_exchange'
+                channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
 
-        channel.queue_declare(queue='connect')
-        channel.queue_declare(queue=self.name)
-        channel.queue_declare(queue=f'{self.name}_id')
+                cr = messages.ConnectionRequest(queue_name=f'{self.name}', type=messages.DEVICE_TYPE_ACTUATOR, ip=self.ip, port=self.port)
 
-        cr = messages.ConnectionRequest(queue_name=self.name, type=messages.DEVICE_TYPE_SENSOR)
+                register_queue = 'connect'
+                channel.queue_declare(queue=register_queue)
+                channel.basic_publish(exchange='',
+                                      routing_key=register_queue,
+                                      body=cr.SerializeToString())
 
-        channel.basic_publish(exchange='',
-                              routing_key='connect',
-                              body=cr.SerializeToString())
+                result = channel.queue_declare('')
+                registration_order_queue = result.method.queue
+                channel.queue_bind(exchange=exchange_name, queue=registration_order_queue)
 
+                for method_frame, props, body in channel.consume(registration_order_queue,  auto_ack=True):
+                    logger.info("Registration order arrived")
+                    break
+        except Exception as e:
+            logger.error(e)
+        finally:
+            channel.close()
 
-        for method_frame, props, body in channel.consume(f'{self.name}_id',  auto_ack=True):
-            self.id = messages.ConnectionResponse.FromString(body).id
-            break
-
-        channel.close()
 
     def send_to_gateway(self):
-        channel = self.connection.channel()
+        connection: pika.BlockingConnection = connect_to_broker() 
+        channel = connection.channel()
+        channel.queue_declare(queue='sensor_updates')
+        logger.info("Starting sending to gateway")
 
         try:
             while True:
-                sdu = messages.SensorDataUpdate(data=str(self.data),id=self.id)
+                sdu = messages.SensorDataUpdate(
+                    data=str(self.data),
+                    id=self.sensor_id,
+                    name=self.name,
+                )
                 self.data_lock.acquire()
-                logger.info("Sending data to gateway")
+                queue = f'sensor_updates'
+                logger.info(f"Sending data to gateway at {queue}")
                 logger.info(f"Data: {sdu.data}")
                 logger.info(f"ID: {sdu.id}")
 
                 channel.basic_publish(exchange='',
-                                      routing_key=queue_name,
+                                      routing_key=queue,
                                       body=sdu.SerializeToString())
                 self.data_lock.release()
 
@@ -82,35 +109,11 @@ class Device():
             channel.close()
 
     def change_data(self, request: services.ActuatorState) -> int | None:
-        if request.id != self.id:
-            logger.warning("ID does not match")
-            return None
-
         self.data_lock.acquire()
         self.data = int(request.state)
         self.data_lock.release()
 
         return self.data
-
-
-    def disconnect(self):
-        channel = self.connection.channel()
-
-        channel.queue_declare("disconnect")
-
-        dr = messages.DisconnectionRequest(id=self.id, queue_name=self.name)
-
-        logger.info("Sending disconnect message")
-
-        channel.basic_publish(
-            "",
-            "disconnect",
-            dr.SerializeToString()
-        )
-
-        channel.close()
-        self.connection.close()
-
 
 
 def handle_signal(signum, frame):
@@ -123,26 +126,27 @@ if __name__ == '__main__':
     for sig in signals:
         signal.signal(sig, handle_signal)
 
-    if len(sys.argv) <= 1:
-        print("Usage: python main.py <queue_name>")
+    if len(sys.argv) <= 3:
+        print("Usage: python main.py <queue_name> <ip> <port>")
         sys.exit(1)
 
     queue_name = sys.argv[1]
+    ip = sys.argv[2]
+    port = sys.argv[3]
 
     logger = logging.LoggerAdapter(logger, {"app_name":queue_name})
 
-    device = Device(queue_name, 10)
-
-    atexit.register(device.disconnect)
+    device = Device(queue_name, ip, port, 10)
 
     try:
         device.start()
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        server.add_insecure_port("[::]:50051")
+        server.add_insecure_port(f"[::]:{port}")
         services_grpc.add_ActuatorServicer_to_server(actuator.ActuatorServer(device), server)
         server.start()
         server.wait_for_termination()
-    except Exception:
+    except Exception as err:
+        logger.error(err)
         pass
     finally:
         sys.exit(0)
