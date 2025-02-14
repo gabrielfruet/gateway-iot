4b10c7f8c8a6c804ce5503ce3eea67055f3f66e0
gabrielfruet <gabrielfruet538@gmail.com>
2025-02-14 18:34:44 -0300

    feat: correct ip

--- devices/launch.sh
+++ devices/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python "./main.py" arconditioner-1 localhost 4999 &
-python "./main.py" arconditioner-2 localhost 4998 & 
-python "./main.py" arconditioner-3 localhost 4997 &
+python "./main.py" arconditioner-1 "$(hostname -i)" 4999 &
+python "./main.py" arconditioner-2 "$(hostname -i)" 4998 & 
+python "./main.py" arconditioner-3 "$(hostname -i)" 4997 &
