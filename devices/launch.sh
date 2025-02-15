7dc1113d18306a37ffdb8753834b6ea396b9a2d8
gabrielfruet <gabrielfruet538@gmail.com>
2025-02-15 10:32:37 -0300

    feat: pid are now tracked and properly killed

--- devices/launch.sh
+++ devices/launch.sh
@@ -1,5 +1,35 @@
-#!/bin/sh
+#!/bin/env bash
 
-python "./main.py" arconditioner-1 "$(hostname -i)" 4999 &
-python "./main.py" arconditioner-2 "$(hostname -i)" 4998 & 
-python "./main.py" arconditioner-3 "$(hostname -i)" 4997 &
+devices=(
+    "arconditioner-1" 
+    "arconditioner-2" 
+    "arconditioner-3" 
+    "light-1" 
+    "temperature_sensor-2" 
+    "door_lock-3"
+)
+
+base_port=5123
+
+pids=()
+
+for i in "${!devices[@]}"; do
+    device_name="${devices[$i]}"
+    port="$((i + base_port))"
+    hostname="$(hostname -i | cut -d' ' -f1)"
+    python main.py "$device_name" "$hostname" "$port" &
+    pids+=( $! )
+done
+
+on_exit() {
+    for i in "${!pids[@]}"; do
+        kill "${pids[$i]}"
+        echo "Killing device ${devices[$i]} of pid ${pids[$i]}"
+    done
+}
+
+trap on_exit EXIT
+
+for pid in "${pids[@]}"; do
+    wait "$pid"
+done
